03451292f73ad05425020b5981aa16eae7d7aa44
Antonio Maldonado III <motionscubed@gmail.com>
2024-07-19 03:05:53 +0100

    Add search & open function + QOL

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-import subprocess as sp
 import pyperclip
+import webbrowser
+import json
+
+# Change file destination
+FILE_PATH = "data.json"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -35,25 +40,59 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
-    if website == '' or email == '' or password == '':
+    # You can use '' or 0 to define a error, or len() to capture a limit.
+    if website == '' or email == '' or len(password) == 0:
         messagebox.showinfo(title="Error", message="Please fill all fields")
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message="Save details?")
+        try:
+            with open(FILE_PATH, "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open(FILE_PATH, "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
-        if is_ok:
-            messagebox.showinfo(title=website, message="Save complete.")
-            with open("data.txt", "a") as data_file:
-                data_file.write(f'{website} | {email} | {password}\n')
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            with open(FILE_PATH, "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
-def open_password():
-    program_name = "notepad.exe"
-    file_name = "data.txt"
-    sp.Popen([program_name, file_name])
+def search_password():
+    website = website_entry.get()
+    try:
+        with open(FILE_PATH) as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="Data file was not found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="Search", message=f"Email: {email}\nPassword: {password}")
+
+        else:
+            messagebox.showerror(title="Error", message=f"No details for {website} exist.")
+
+
+def open_document():
+    try:
+        webbrowser.open('file://' + os.path.realpath(FILE_PATH))
+    except webbrowser.Error:
+        messagebox.showerror(title="Error", message="File not found.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -76,9 +115,12 @@ website_label = Label(text='Website:')
 website_label.grid(row=1, column=0)
 website_label.config(font=('Arial', 8))
 
-website_entry = Entry(width=43)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=33)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
+
+website_button = Button(text='  Search  ', command=search_password)
+website_button.grid(row=1, column=2)
 
 # Email Entry
 email_label = Label(text='Email/Username:')
@@ -106,11 +148,11 @@ add_button = Button(text='Add', command=save_password)
 add_button.config(width=36)
 add_button.grid(row=4, column=1, columnspan=2)
 
-# Open Saved Entries
+# Open Document Button
 
-open_button = Button(text='Open Data Manager', command=open_password)
-open_button.config(width=36)
-open_button.grid(row=5, column=1, columnspan=2)
+exit_button = Button(text='Open Document', command=open_document)
+exit_button.config(width=36)
+exit_button.grid(row=5, column=1, columnspan=2)
 
 # Exit Button
 
